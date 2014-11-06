6888ed6d5e95ed98558ffbe5d5099d245488d13b
Haojian Wu <hokein.wu@gmail.com>
2014-11-06 14:44:17 +0800

    Sync submodules in bootstrap.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -7,6 +7,13 @@ function create_link_force {
   ln -sf "$SOURCE/$1" "$TARGET/$2"
 }
 
+function update_submodules {
+  cd "$SOURCE"
+  git submodule sync
+  git submodule update --init --recursive
+}
+
+update_submodules
 create_link_force vim/.vim .vim
 create_link_force vim/.vimrc .vimrc
 create_link_force git/.gitconfig .gitconfig
